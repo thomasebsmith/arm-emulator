02dbb00019cf19a451a0ac183cc75e4e437f0620
Thomas Smith <thsm@umich.edu>
2020-12-13 23:54:39 -0500

    Add basic CLI option parsing

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -1,20 +1,79 @@
+#include <cassert>
 #include <iostream>
 #include <optional>
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <type_traits>
+#include <variant>
 #include <vector>
 #include "cli/cli.h"
+
+
+template <typename T>
+constexpr bool false_v = false;
 
 CLI::CLI::ParseException::ParseException(const std::string &error_message):
   std::runtime_error{error_message}
 {}
 
 CLI::CLI::CLI(int argc, char *argv[], const std::vector<Option> &options) {
-  (void) argc;
-  (void) argv;
-  (void) options;
-  throw ParseException("This is not yet implemented.");
+  if (argc == 0) {
+    throw ParseException("No program name given");
+  }
+
+  for (const auto &option_variant : options) {
+    std::visit([=](auto &&option) {
+      using OptionT = std::decay_t<decltype(option)>;
+      if constexpr (std::is_same_v<OptionT, Argument>) {
+        possible_arguments.push_back(option);
+      }
+      else if constexpr (std::is_same_v<OptionT, Flag>) {
+        if (!possible_flags.insert({option.full_name, option}).second) {
+          throw ParseException("Repeated option " + option.full_name);
+        }
+        if (!flag_shortcuts.insert({
+          option.shortcut,
+          option.full_name
+        }).second) {
+          throw ParseException(std::string("Repeated option shortcut ") + option.shortcut);
+        }
+      }
+      else {
+        static_assert(false_v<OptionT>, "Unexpected CLI option variant");
+      }
+    }, option_variant);
+  }
+
+  bool raw_arguments = false;
+  for (int i = 1; i < argc; ++i) {
+    const std::string &argument = argv[i];
+    if (raw_arguments || argument == "-" || argument.rfind("-", 0) != 0) {
+      assert(arguments.size() <= possible_arguments.size());
+      if (arguments.size() == possible_arguments.size()) {
+        throw ParseException("Unnecessary argument " + argument);
+      }
+      arguments.push_back(argument);
+    }
+    else if (argument == "--") {
+      raw_arguments = true;
+    }
+    else {
+      assert(argument.length() > 1);
+      assert(argument[0] == '-');
+      if (argument[1] == '-') {
+        // Option begins with --
+        assert(argument.length() > 2);
+        auto flag_name = argument.substr(2);
+        if (possible_flags.find(flag_name) == possible_flags.end()) {
+          throw ParseException("Unknown flag " + argument);
+        }
+        if (!flags.emplace(std::move(flag_name)).second) {
+          throw ParseException("Repeated flag " + argument);
+        }
+      }
+    }
+  }
 }
 
 bool CLI::CLI::has_flag(const std::string_view full_name) {
--- src/cli/cli.h
+++ src/cli/cli.h
@@ -6,6 +6,8 @@
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <unordered_map>
+#include <unordered_set>
 #include <variant>
 #include <vector>
 
@@ -14,22 +16,22 @@ namespace CLI {
   public:
 
     struct Argument {
-      const std::string_view internal_name;
-      const std::string_view help_name;
-      const std::string_view description;
+      const std::string internal_name;
+      const std::string help_name;
+      const std::string description;
 
       Argument() = delete;
     };
 
     struct Flag {
       char shortcut;
-      const std::string_view full_name;
-      const std::string_view description;
+      const std::string full_name;
+      const std::string description;
 
       Flag() = delete;
     };
 
-    class ParseException: std::runtime_error {
+    class ParseException: public std::runtime_error {
     public:
       ParseException(const std::string &error_message);
     };
@@ -47,6 +49,12 @@ namespace CLI {
     std::ostream &show_help(std::ostream &out);
 
   private:
+    std::vector<Argument> possible_arguments;
+    std::unordered_map<char, std::string> flag_shortcuts;
+    std::unordered_map<std::string, Flag> possible_flags;
+    std::string program_name;
+    std::vector<std::string> arguments;
+    std::unordered_set<std::string> flags;
   };
 };
 
