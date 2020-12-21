068642a4fe3198b54f71e39c720777fd25f24ea9
Thomas Smith <thsm@umich.edu>
2020-12-20 23:54:33 -0500

    Add outline for test framework

---
+++ test/shared/test_framework.h
@@ -0,0 +1,32 @@
+#ifndef __TEST_SHARED_TEST_FRAMEWORK_H__
+#define __TEST_SHARED_TEST_FRAMEWORK_H__
+
+#include <function>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+// TODO: Test.h
+
+class TestFramework {
+public:
+  class FailedAssertion: std::runtime_error {
+  public:
+    FailedAssertion(const std::string &msg);
+  };
+
+  void that(
+    const std::string &description,
+    const std::function<void()> &run_test
+  );
+
+  void include(TestFramework &&otherFramework);
+
+  static void assert(bool value, const std::string &failure_message);
+
+private:
+  std::vector<TestFramework> subFrameworks;
+  std::vector<Test> tests;
+}
+
+#endif
