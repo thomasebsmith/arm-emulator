4da5b14e2eb99122dc81d1c23595d7aa19c77ea9
Thomas Smith <thsm@umich.edu>
2021-01-01 23:43:45 -0500

    Add inline documentation to Disassembler class

--- src/disassemble/disassembler.h
+++ src/disassemble/disassembler.h
@@ -8,14 +8,31 @@
 #include "program.h"
 
 namespace Disassemble {
+  /*
+   * Represents some machine code that is in the process of being disassembled.
+   */
   class Disassembler {
   public:
+
+    /*
+     * Disassembles the machine code found in machine_code. machine_code is
+     * read as binary data.
+     *
+     * May throw a ParseException.
+     */
     Disassembler(std::istream &machine_code);
 
+    /*
+     * Prints a human-readable, disassembled version of the machine code to
+     * out.
+     */
     void print_to(std::ostream &out);
 
     using DecodeException = Instructions::DecodeException;
 
+    /*
+     * Represents an error that occurs while parsing the machine code.
+     */
     class ParseException: public std::runtime_error {
     public:
       ParseException(const std::string &error_message);
@@ -25,6 +42,9 @@ namespace Disassemble {
     Program program;
   };
 
+  /*
+   * Equivalent to disassembler.print_to(out).
+   */
   std::ostream &operator<<(std::ostream &out, Disassembler &disassembler);
 }
 
