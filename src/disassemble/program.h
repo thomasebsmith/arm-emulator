32ecbc54734fa4378b599d80235ee42c8fe769c6
Thomas Smith <thsm@umich.edu>
2021-01-02 23:51:29 -0500

    Add inline documentation to Program class

--- src/disassemble/program.h
+++ src/disassemble/program.h
@@ -7,13 +7,24 @@
 #include "instructions/shared.h"
 
 namespace Disassemble {
+  /*
+   * Represents a series of ARM instructions.
+   * Initially, it is empty.
+   */
   class Program {
   public:
     using Instruction = Instructions::Instruction;
     using InstructionType = Instructions::IntegerType;
 
+    /*
+     * Appends inst to this Program. inst should be an ARM instruction, encoded
+     * in its binary machine code form.
+     */
     void add_instruction(InstructionType inst);
 
+    /*
+     * Prints disassembly for all instructions in this Program to out.
+     */
     void print_to(std::ostream &out) const;
   private:
     std::vector<Instruction> instructions;
