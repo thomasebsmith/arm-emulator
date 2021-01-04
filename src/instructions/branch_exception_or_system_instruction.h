2b588a24e1a27fa06e2111cbe5e3673574831be7
Thomas Smith <thsm@umich.edu>
2021-01-03 23:53:41 -0500

    Document branch/exception/system instruction class

--- src/instructions/branch_exception_or_system_instruction.h
+++ src/instructions/branch_exception_or_system_instruction.h
@@ -5,9 +5,21 @@
 #include "shared.h"
 
 namespace Instructions {
+  /*
+   * Represents an ARM instruction that performs a branch, is exeception-
+   * related, or is system-related.
+   */
   class BranchExceptionOrSystemInstruction {
   public:
+    /*
+     * Parses an instruction from the binary data in inst. Requires that inst
+     * represetns an instruction of the appropriate category.
+     */
     BranchExceptionOrSystemInstruction(IntegerType inst);
+
+    /*
+     * Outputs a human-readable (disassembled) form of the instruction to out.
+     */
     void print_to(std::ostream &out) const;
   };
 }
