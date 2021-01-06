aa31fb7f13e0c32f3950c2c53ecd793d52f8bd5b
Thomas Smith <thsm@umich.edu>
2021-01-05 23:33:09 -0500

    Document immediate data-processing instructions

--- src/instructions/data_processing_immediate_instruction.h
+++ src/instructions/data_processing_immediate_instruction.h
@@ -5,9 +5,21 @@
 #include "shared.h"
 
 namespace Instructions {
+  /*
+   * Represents an ARM instruction that performs a data-processing instruction
+   * involving an immediate (literal) value.
+   */
   class DataProcessingImmediateInstruction {
   public:
+    /*
+     * Parses an instruction from the binary data in inst. Requires that inst
+     * represents an instruction of the appropriate category.
+     */
     DataProcessingImmediateInstruction(IntegerType inst);
+
+    /*
+     * Outputs a human-readable (disassembled) form of the instruction to out.
+     */
     void print_to(std::ostream &out) const;
   };
 }
