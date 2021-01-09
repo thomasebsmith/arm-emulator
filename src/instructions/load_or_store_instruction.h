0254b3c14b11d0eb0c8c82b0d95c2c58b02f7b16
Thomas Smith <thsm@umich.edu>
2021-01-08 23:47:22 -0500

    Document LoadOrStoreInstruction class

--- src/instructions/load_or_store_instruction.h
+++ src/instructions/load_or_store_instruction.h
@@ -5,9 +5,20 @@
 #include "shared.h"
 
 namespace Instructions {
+  /*
+   * Represents an ARM instruction that performs a load or store operation.
+   */
   class LoadOrStoreInstruction {
   public:
+    /*
+     * Parses an instruction from the binary data in inst. Requires that inst
+     * represents an instruction of the appropriate category.
+     */
     LoadOrStoreInstruction(IntegerType inst);
+
+    /*
+     * Outputs a human-readable (disassembled) form of the instruction to out.
+     */
     void print_to(std::ostream &out) const;
   };
 }
