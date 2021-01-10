05e3aaf2abd498b2aa1d84b73d1cba5df57249a3
Thomas Smith <thsm@umich.edu>
2021-01-09 23:44:47 -0500

    Add comments to ReservedInstruction class

--- src/instructions/reserved_instruction.h
+++ src/instructions/reserved_instruction.h
@@ -5,9 +5,22 @@
 #include "shared.h"
 
 namespace Instructions {
+  /*
+   * Represents an ARM instruction that is reserved. Such an instruction does
+   * not perform a valid operation (the only valid instruction within this
+   * class is UDF).
+   */
   class ReservedInstruction {
   public:
+    /*
+     * Parses an instruction from the binary data in inst. Requires that inst
+     * represents an instruction of the appropriate category.
+     */
     ReservedInstruction(IntegerType inst);
+
+    /*
+     * Outputs a human-readable (disassembled) form of the instruction to out.
+     */
     void print_to(std::ostream &out) const;
   private:
     enum class Type {
