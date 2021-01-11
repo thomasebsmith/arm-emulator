44462853c1cac519c74d7be562eb9ff383454a95
Thomas Smith <thsm@umich.edu>
2021-01-10 23:46:38 -0500

    Document SVEInstruction class

--- src/instructions/sve_instruction.h
+++ src/instructions/sve_instruction.h
@@ -5,9 +5,21 @@
 #include "shared.h"
 
 namespace Instructions {
+  /*
+   * Represents an ARM instruction that performs a scalable vector extension
+   * instruction.
+   */
   class SVEInstruction {
   public:
+    /*
+     * Parses an instruction from the binary data in inst. Requires that inst
+     * represents an instruction of the appropriate category.
+     */
     SVEInstruction(IntegerType inst);
+
+    /*
+     * Outputs a human-readable (disassembled) form of the instruction to out.
+     */
     void print_to(std::ostream &out) const;
   };
 }
