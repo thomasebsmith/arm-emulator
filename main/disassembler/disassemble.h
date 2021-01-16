37acb6196f702ee729aaf965a3c02566b4fbfe39
Thomas Smith <thsm@umich.edu>
2021-01-15 23:50:37 -0500

    Add documentation for disassemble function

--- main/disassembler/disassemble.h
+++ main/disassembler/disassemble.h
@@ -3,6 +3,11 @@
 
 #include <iostream>
 
+/*
+ * Disassembles the machine code contained in in, writing output to out.
+ * If any errors occur, they are written to out_err and a non-zero exit code
+ * is returned.
+ */
 int disassemble(std::istream &in, std::ostream &out, std::ostream &out_err);
 
 #endif
