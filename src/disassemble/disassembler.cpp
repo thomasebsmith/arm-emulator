c3ffd639387a7600b157aa94fb42098093b566bc
Thomas Smith <thsm@umich.edu>
2021-01-22 23:43:54 -0500

    Add comments to disassembler.cpp

--- src/disassemble/disassembler.cpp
+++ src/disassemble/disassembler.cpp
@@ -9,6 +9,9 @@ namespace Disassemble {
   Disassembler::Disassembler(std::istream &machine_code): in{machine_code} {}
 
   void Disassembler::print_to(std::ostream &out) {
+    // Read the raw bytes of each instruction into inst.
+    // Then, add inst to a program.
+    // Finally, print the entire program.
     Program::InstructionType inst;
     while (in.read(
       reinterpret_cast<char *>(&inst),
@@ -20,6 +23,7 @@ namespace Disassemble {
     program.print_to(out);
   }
 
+  // ParseException is just a wrapper for std::runtime_error.
   Disassembler::ParseException::ParseException(
     const std::string &error_message
   ): std::runtime_error{error_message} {}
