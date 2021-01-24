4cd06ad0d8ddd36a1c8870104f64a639857e3c07
Thomas Smith <thsm@umich.edu>
2021-01-23 23:23:42 -0500

    Add comments to shared instructions header

--- src/instructions/shared.h
+++ src/instructions/shared.h
@@ -4,7 +4,12 @@
 #include <cstdint>
 
 namespace Instructions {
+  // IntegerType designates the type of integer that holds exactly one ARM
+  // instruction.
   using IntegerType = uint32_t;
+
+  // ImmediateType designates the type of integer that holds exactly one ARM
+  // immediate value (part of certain instructions).
   using ImmediateType = uint16_t;
 }
 
