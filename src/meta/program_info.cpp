b5540c68ca7bdb403b58a321fb9901e1921c6e9d
Thomas Smith <thsm@umich.edu>
2023-02-11 23:47:09 -0500

    Update copyright to include 2023

--- src/meta/program_info.cpp
+++ src/meta/program_info.cpp
@@ -4,7 +4,7 @@
 std::string Meta::ProgramInfo::name = "ARM Emulator";
 std::string Meta::ProgramInfo::version = "0.0";
 std::string Meta::ProgramInfo::author = "Thomas Smith";
-std::string Meta::ProgramInfo::copyright = "2020";
+std::string Meta::ProgramInfo::copyright = "2023";
 std::string Meta::ProgramInfo::license = "the MIT License";
 
 std::ostream &Meta::ProgramInfo::show_version_info(std::ostream &out) {
